ed0da4918c749b38ad67046330135a9e2df1dcd0
aniket866 <iamaniketkumarmaner@gmail.com>
2026-02-28 20:41:11 +0530

    Final-commit

--- src/config/site.ts
+++ src/config/site.ts
@@ -1,7 +1,8 @@
 export const siteConfig = {
   name: "TODO: Project Name",
-  description: "TODO: A proper SEO description explaining what this project does.",
-  url: "https://TODO:PROJECT-NAME.stability.nexus",
+  description:
+    "TODO: A proper SEO description explaining what this project does.",
+  url: "https://example.com", // TODO: Replace with production domain
   ogImage: "/og-image.jpg",
   author: "Stability Nexus",
 };
